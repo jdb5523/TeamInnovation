2d8d7576864eab5f1fa505c51005e3748e34b311
sja5262 <32081041+sja5262@users.noreply.github.com>
2017-11-04 04:40:03 -0400

    Update Caesar.java

--- src/Cipher/Caesar.java
+++ src/Cipher/Caesar.java
@@ -3,82 +3,31 @@ package Cipher;
 import java.util.Locale;
 import javax.swing.*;
 
+
 public class Caesar {
 
     public String decryptCaesar(String informedcode) {
-        String message, decryptedMessage = "", transMessage, finalTrans, check;
-        int key, k;
-        double ratio, ratTotal, length;
+        String message, decryptedMessage = "", finalResult, loopInfo;
+        int key;
+        char character;
         GoogleTranslate trans;
         String language;
         JTextArea allResults;
+
 
         message = informedcode;
         allResults = new JTextArea("");
         trans = new GoogleTranslate();
         language = new String("");
-        transMessage = new String("");
         String langName = new String("");
         StringBuilder resultPool = new StringBuilder("");
-        finalTrans = new String("");
-        check = new String("");
-        ratio = 0;
-        k = 0;
-        ratTotal = 0;
-        length = 0;
-        
-        
-        language = trans.detectLanguage(message);
-            
-            Locale loc = new Locale(language);
-            langName = loc.getDisplayLanguage(loc);
-            
-           if ("English".equals(langName))
-           {
-               String[] splitMessage = message.split("\\s+");
-                length = splitMessage.length;
-                System.out.println(length);
-            
-                for (int i = 0; i < splitMessage.length; i++) {
-          
-                check = trans.checkEnglish(splitMessage[i]);
-                System.out.println(check);
-                
-                    if("WORD FOUND".equals(check)){
-                    ratTotal++;}
-                
-                resultPool.append(transMessage+" ");
-                k++;
-                }
-               
-               System.out.println(ratTotal);
-               ratio = ratTotal/length;
-               System.out.println(ratio);
-               finalTrans = message.toUpperCase();
-           }
-           else
-           {
-            String[] splitMessage = message.split("\\s+");
-            System.out.println(splitMessage.length);
-            
-            k = 0;
-            
-            for (int i = 0; i < splitMessage.length; i++) {
-                
-                transMessage = trans.translateLanguage(splitMessage[k], language);
-                resultPool.append(transMessage+" ");
-                k++;
-                }
-            
-            finalTrans = resultPool.toString();
-            finalTrans = finalTrans.toUpperCase();
-            }
+        finalResult = new String("");
 
-        allResults.append("Language: "+ langName +" - Message: "+finalTrans);
-/*
+        
         for (key = 1; key < 27; ++key) {
             decryptedMessage = "";
             for (int i = 0; i < message.length(); ++i) {
+                
                 character = message.charAt(i);
                 if (character >= 'a' && character <= 'z') {
                     character = (char) (character - key);
@@ -98,14 +47,27 @@ public class Caesar {
                 } else {
                     decryptedMessage += character;
                 } 
-                language = trans.detectLanguage(decryptedMessage);
 
+            }          
+            
+            language = trans.detectLanguage(decryptedMessage);
+            
+            Locale loc = new Locale(language);
+            langName = loc.getDisplayLanguage(loc);
+            
+            finalResult = trans.runProcess(language, decryptedMessage, langName);
+
+            if ("LOW RATIO".equals(finalResult))
+            {
+                allResults.append("");
             }
-            allResults.append("Caesar Key " + key + ":\n" + decryptedMessage + "\n");
-        }
-//}
-        message = allResults.getText();
-       */ return allResults.getText();
+            else{
+                allResults.append("Caesar Cipher Key " + key + ":\n"+"Decrypted Message: " + decryptedMessage + "\n"
+                    + "Language: "+ langName +"\n"+" - FINAL RESULT: "+ finalResult+"\n"+"\n"+"\n");
+            }
+            
+    }
+       return allResults.getText();
     }
     
 }
