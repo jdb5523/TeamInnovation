fa766d6fae470ad73fe9df13cab95ac6948dda1e
sja5262 <32081041+sja5262@users.noreply.github.com>
2017-11-08 18:34:35 -0500

    Update Decoder.java

--- src/Cipher/Decoder.java
+++ src/Cipher/Decoder.java
@@ -12,17 +12,22 @@ public class Decoder {
         StringBuilder resultPool;
         Caesar caesarCipher;
         Atbash atbashCipher;
+        Affine affineCipher;
         
         allResults = new String("");
         
         message = informedcode;
         caesarCipher = new Caesar();
         atbashCipher = new Atbash();
+        affineCipher = new Affine();
         
         result = caesarCipher.decryptCaesar(message);
         resultPool = new StringBuilder(result);
         result = atbashCipher.decryptAtbash(informedcode);
         resultPool.append(result);
+        result = affineCipher.decryptAffine(informedcode);
+        resultPool.append(result);
+
         
         
         allResults = resultPool.toString();
